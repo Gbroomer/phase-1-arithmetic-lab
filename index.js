e46841428889b5cd2b7a421d4ba4e27f37985325
Grant Bruemmer <grantbruemmer@gmail.com>
2023-06-12 13:48:30 -0500

    Complete Arithmetic Lab

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-
 // Write your code here
+const num1 =  2, num2 = 31, num3 = 16, num4 = 4
+const num5 = [5, 10, 15, 20]
+let multiply = (num1 * num2);
+let mod = (num3 / num4);
+
+let random = Math.floor(Math.random()) + 1;
+let max = Math.max(...num5);
